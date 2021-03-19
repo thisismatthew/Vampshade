3460c3ba79e553fcc4c1815bae34e8b4967f6c7b
Matthew Jackson <102381833@student.swin.edu.au>
2021-03-19 12:39:25 +1100

    + camera controller fixed + shadow collider now "shades" the vamp material on enter

--- VampireGame/Assets/Scripts/CameraController.cs
+++ VampireGame/Assets/Scripts/CameraController.cs
@@ -7,7 +7,7 @@ public class CameraController : MonoBehaviour
     // so i want a camera controller where the player can move around a bit without the camera moving,
     // like get to the edges and then the camera lerps over a bit. 
 
-    const float MAX_DISTANCE = 1f;
+    public const float MAX_DISTANCE = 3f;
 
     public Transform target; // Specify the player
     public float speed = 1f;
@@ -27,13 +27,18 @@ public class CameraController : MonoBehaviour
     {
         if (currentPos != projectedPos)
         {
-            Vector2.MoveTowards(currentPos, projectedPos, speed);
+            Vector3 newPos = Vector2.Lerp(currentPos, projectedPos, Time.deltaTime * speed);
+            newPos.z = -10;
+            transform.position = newPos;
             currentPos = transform.position;
         }
 
         if (Vector2.Distance(currentPos, target.transform.position) > MAX_DISTANCE)
         {
             projectedPos = target.transform.position;
+            //this line is just to offset the pivot being at the players feet
+            projectedPos.y += 5f;
+            
         }
 
 
--- VampireGame/Assets/Scripts/ShadowCollider.cs
+++ VampireGame/Assets/Scripts/ShadowCollider.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class ShadowCollider : MonoBehaviour
 {
+    public GameObject vamp;
+    public Color startColor;
+    public Color shadedColor;
+
+    private void Start()
+    {
+        startColor = vamp.GetComponent<Renderer>().material.GetColor("_Color");
+
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         collision.transform.GetComponent<Player>().shaded = true;
-        Debug.Log("Shaded");
+        vamp.GetComponent<Renderer>().material.SetColor("_Color", shadedColor);
+        //Debug.Log("Shaded");
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         collision.transform.GetComponent<Player>().shaded = false;
+        vamp.GetComponent<Renderer>().material.SetColor("_Color", startColor);
     }
 }
